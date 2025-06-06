10d214840ab625f0f87e19d31978ed49b2071e76
ViaemjedS <1767703438@qq.com>
2025-06-06 23:40:37 +0800

    添加了关于map面试题的代码

--- batjtmd/js/map/2.js
+++ batjtmd/js/map/2.js
@@ -1,4 +1,5 @@
-// parseInt num 
+// parseInt num
+
 ['1', '2', '3'].map((num, index, arr) => {
     console.log(num, index, arr);
     return num;
@@ -8,4 +9,4 @@
 // 第三个不用
 console.log(parseInt('1', 0, ['1', '2', '3']));
 console.log(parseInt('2', 1, ['1', '2', '3']));
-console.log(parseInt('2', 2, ['1', '2', '3']));
+console.log(parseInt('3', 2, ['1', '2', '3']));
