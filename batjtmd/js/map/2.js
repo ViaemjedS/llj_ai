de3a66f94f32d4d1a0355323b684e5e49e1c0f96
ViaemjedS <1767703438@qq.com>
2025-06-06 22:02:27 +0800

    学习了parseInt有关面试题，以及原型机制

---
+++ batjtmd/js/map/2.js
@@ -0,0 +1,11 @@
+// parseInt num 
+['1', '2', '3'].map((num, index, arr) => {
+    console.log(num, index, arr);
+    return num;
+})
+
+// 第二个参数是进制数，默认是10进制
+// 第三个不用
+console.log(parseInt('1', 0, ['1', '2', '3']));
+console.log(parseInt('2', 1, ['1', '2', '3']));
+console.log(parseInt('2', 2, ['1', '2', '3']));
---
+++ batjtmd/js/prototype/Puppy.java
@@ -0,0 +1,12 @@
+public class Puppy {
+    int pupyAge;
+    public Puppy(int age) {
+        pupyAge = age;
+    }
+    //公有方法
+    public void say() {
+        System.out.println("汪汪汪");
+    }
+
+    
+}
