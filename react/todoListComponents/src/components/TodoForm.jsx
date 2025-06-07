bc13f490cc6831608a6bd3ce2b2b6efa838373de
ViaemjedS <1767703438@qq.com>
2025-06-07 17:26:01 +0800

    学习了react的响应式数据状态

--- react/todoListComponents/src/components/TodoForm.jsx
+++ react/todoListComponents/src/components/TodoForm.jsx
@@ -1,7 +1,30 @@
-function TodoForm() {
-    return (
-        <form>
+import { useState } from 'react';
+function TodoForm(props) {
+    const onAdd = props.onAdd;
+    const [text, setText] = useState('');
 
+    const handleSubmit = (e) => {
+        // 阻止默认行为
+        // 由js 来控制
+        e.preventDefault(); // event api
+        console.log(text);
+        onAdd(text);
+        // todos? 打报告
+    }
+    const handleChange = (e) => {
+        setText(e.target.value);
+    }
+    return (
+        <form action="http://www.baidu.com" onSubmit={handleSubmit}>
+            
+            <input 
+            // 一定要写placeholder 增强语义化
+                type="text" 
+                placeholder="请输入代办事项"
+                value={text}
+                onChange={handleChange}
+            />
+            <button type="submit">添加</button>
         </form>
     )
 }
