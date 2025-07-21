84723c91943502272c6c6afcfda9cd883b6bcbf2
ViaemjedS <1767703438@qq.com>
2025-07-21 17:04:42 +0800

    feat: 新增React面试演示项目和CSS模块化示例
    添加了两个React示例项目：
    1. forward-demo: 展示React forwardRef的使用
    2. css-bug-demo: 演示CSS模块化解决方案
    
    包含完整的项目结构、配置文件和示例代码

---
+++ react/interview/css/css-bug-demo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Button from './components/Button'
+import AnotherButton from './components/AnotherButton'
+// import 运行
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <AnotherButton />
+      <Button />
+    </>
+  )
+}
+
+export default App
