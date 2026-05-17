0dd7e4bf5330224be88cb8c00d00f0b2cf0f4f80
ViaemjedS <1767703438@qq.com>
2026-05-17 22:13:37 +0800

    chore: 初始化nestjs+langchain项目基础框架
    初始化完整的NestJS项目结构，包含基础的CRUD模块、AI聊天模块、配置与静态文件服务，添加了代码规范配置、测试用例和项目文档

---
+++ ai/langchain_nest/hello-nest-langchain2/src/ai/ai.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { ChatOpenAI } from '@langchain/openai';
+import { PromptTemplate } from '@langchain/core/prompts';
+import { StringOutputParser } from '@langchain/core/output_parsers';
+import { Runnable } from '@langchain/core/runnables';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AiService {
+    // 链式调用对象
+    private readonly chain: Runnable;
+
+    constructor(@Inject(ConfigService) configService: ConfigService) {
+        const prompt = PromptTemplate.fromTemplate(
+            `
+                请回答一下问题： \n\n{query}
+            `
+        );
+        const model = new ChatOpenAI({
+            temperature: 0.7,
+            modelName: configService.get('MODEL_NAME'),
+            apiKey: configService.get('OPENAI_API_KEY'),
+            configuration: {
+                baseURL: configService.get('OPENAI_BASE_URL'),
+            }
+        });
+        this.chain = prompt.pipe(model).pipe(new StringOutputParser());
+    }
+    async runChain(query: string) : Promise<string>{
+        return this.chain.invoke({ query });
+    }
+}
