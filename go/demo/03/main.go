3d0b799a5173a628dd91bca248b79a61d35efe3f
ViaemjedS <1767703438@qq.com>
2026-05-24 22:48:05 +0800

    docs: add comments and new project docs
    add comments for go demo and server demo code, add fullstack ai project docs

--- go/demo/03/main.go
+++ go/demo/03/main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	// chan 通道 主线程和协程之间通信的通道
 	// 传递数据的类型是整型
+	// 创建一个传输整数的通道，让不同的 goroutine 可以安全地发送和接收数据，实现并发通信！
 	ch := make(chan int)
 
 	go func() {
--- go/server-demo/main.go
+++ go/server-demo/main.go
@@ -11,6 +11,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// 注册路由处理函数 ，当访问指定路径时，执行对应的处理函数。
 	http.HandleFunc("/", handler) 
+	// 启动 HTTP 服务器 ，监听指定端口，接收并处理请求。
 	http.ListenAndServe(":8080", nil)
 }
